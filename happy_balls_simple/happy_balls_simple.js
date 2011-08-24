13e7e7edfb5a33c6db2c1f30766f8d5770819c90
Steve Winton <stevewinton@gmail.com>
2011-08-24 16:06:37 +0100

    Working in Firefox 4, thanks to the magical, marvellous Processing.js! :)

--- happy_balls_simple/happy_balls_simple.js
+++ happy_balls_simple/happy_balls_simple.js
@@ -1,13 +1,14 @@
-int num = 1;
+int num = 0;
 color happy = color(239, 240, 56);
 color unhappy = color(176, 210, 235);
+color clr = externals.canvas.id == "happyBucket" ? happy : unhappy;
 float radius = 40;
 float GRAVITY = 9.8 / frameRate;
 float FRICTION = 0.4;
 // List<LimitedParticle> particles = new ArrayList<LimitedParticle>();
 var particles = [];
 SinkListener sinkListener = new SinkListenerImpl(); 
-boolean addParticles = true;
+boolean addParticles = false;
 
 void setup() {
   size(500, 500);
@@ -16,6 +17,7 @@ void setup() {
   for (int i = 0; i < num; i++) {
     initParticle();
   }
+  println("Running on:  " + externals.canvas.id);
 }
 
 void draw() {
@@ -29,7 +31,7 @@ void draw() {
     }    
   });
   
-  fill(happy);
+  fill(clr);
   rect(0, Floor.y, width, height - Floor.y);
   
   if (addParticles && frameCount % 5 == 0) {
@@ -40,7 +42,7 @@ void draw() {
 void initParticle() {
   float velX = random(-10, 10);
   float velY = 0;//random(6, 6);
-  LimitedParticle p = new LimitedParticle(width/2, 0, velX, velY, radius, happy, GRAVITY, sinkListener);
+  LimitedParticle p = new LimitedParticle(width/2, 0, velX, velY, radius, clr, GRAVITY, sinkListener);
   p.friction = FRICTION;
   particles.push(p);
   println("No. of particles: " + particles.length);
